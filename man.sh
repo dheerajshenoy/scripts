46dbc638c25a11e0954ee6918622abaf8cd1f201
Dheeraj Shenoy <dheerajshenoy22@gmail.com>
2023-02-06 22:42:54 +0530

    dd

--- man.sh
+++ man.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 res=$(man --apropos . | rofi -dmenu -l 20 | awk '{print $1}')
 [ -z "$res" ] && exit
-man --troff-device=pdf $res > /tmp/"$res".pdf | sioyek /tmp/"$res".pdf
+man -t $res | zathura /tmp/"$res".pdf
+#man --troff-device=pdf $res > /tmp/"$res".pdf | zathura /tmp/"$res".pdf
