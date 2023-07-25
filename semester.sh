9a0cca46f5e8e53c03c84bdcc5d7554a64dce019
Dheeraj Shenoy <dheerajshenoy22@gmail.com>
2023-07-25 20:21:29 +0530

    added semester file managing rofi script

---
+++ semester.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+SEMDIR="$HOME/Documents/Sem III"
+
+menuItems="Syllabus
+Time Table"
+
+function syllabus_menu()
+{
+syllabusItems="NPP
+AS
+PS
+Astro
+"
+output=$(echo "$syllabusItems" | rofi -show drun -dmenu )
+
+case "$output" in 
+    "NPP") xdg-open "$SEMDIR/Nuclear and Particle Physics/Syllabus.pdf"
+        ;;
+    "AS") xdg-open "$SEMDIR/Atomic Spectroscopy/Syllabus.pdf"
+
+        ;;
+    "PS") xdg-open "$SEMDIR/Physics of solids/Syllabus.pdf"
+
+        ;;
+    "Astro") xdg-open "$SEMDIR/Astrophysics/Syllabus.pdf"
+
+        ;;
+esac
+
+}
+
+
+output=$(echo "$menuItems" | rofi -show drun -dmenu)
+
+case "$output" in
+    "Syllabus")
+        syllabus_menu
+        ;;
+    "Time Table")
+        xdg-open "$SEMDIR/Time Table.pdf"
+        ;;
+esac
+
