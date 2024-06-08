1ea23a663f878941dab03752437747f407b281f9
Dheeraj Shenoy <dheerajshenoy22@gmail.com>
2024-06-08 22:00:37 +0530

    updated

--- dmenu_emoji.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-chosen=$(cat ~/dotfiles/emojis | rofi -dmenu -no-show-icons | awk '{print $1}' | tr -d ' ')
-
-[ -z "$chosen" ] && exit
-
-printf "$chosen" | xclip -selection clipboard
-notify-send "$chosen copied to clipboard"
--- gsearch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-url="http://www.google.com/search?q="
-
-res=$(rofi -dmenu -p "Google search" -l 0 | sed 's/ /+/g')
-
-[ -z "$res" ] && exit
-
-xdg-open $url$res
